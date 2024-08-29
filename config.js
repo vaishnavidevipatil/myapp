79e0e5e34c21a902be5b55dc71397f9194c95f26
vaishnavidevipatil <145427408+vaishnavidevipatil@users.noreply.github.com>
2024-08-29 09:24:55 +0530

    SignIn/Up the Slide page

---
+++ config.js
@@ -0,0 +1,8 @@
+// webpack.config.js
+module.exports = {
+    // Other configurations...
+    resolve: {
+      symlinks: true, // This enables following symlinks
+    },
+  };
+  
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'; // Adjust the path if your CSS file is located elsewhere
+
+const Homepage = () => {
+  return (
+    <div className="App">
+      {/* Your homepage component code */}
+      <h1>Welcome to the Homepage!</h1>
+      {/* Add more elements as needed */}
+    </div>
+  );
+};
+
+export default Homepage;
